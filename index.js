1441f07a9e1e02b911df8ea7ca0db8d29444652a
Shadab Ansari <shadabansari8319@gmail.com>
2025-06-27 20:37:31 +0530

    formatting

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
 const express = require("express");
-const {connectMongoDb} = require("./connection")
+const { connectMongoDb } = require("./connection");
 const userRouter = require("./routes/user");
-const {logReqRes} = require("./middlewares/")
+const { logReqRes } = require("./middlewares/");
 
 const app = express();
 const PORT = 8000;
 
 // Connection
-connectMongoDb("mongodb://127.0.0.1:27017/app-1").then(()=>{
+connectMongoDb("mongodb://127.0.0.1:27017/app-1").then(() => {
   console.log("MongoDB connected");
-  
 });
 
 //
 app.use(express.urlencoded({ extended: false }));
 app.use(logReqRes("log.txt"));
-app.use("/api/user",userRouter)
-
-// routes
+app.use("/api/user", userRouter);
 
 app.listen(PORT, () => {
   console.log(`Server Started as PORT: ${PORT}`);
